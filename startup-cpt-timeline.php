aef0dd54814a242e2d45249c60718f9129085afd
yozz <yozz.online@gmail.com>
2016-03-21 09:28:31 -0400

    Fontawesome list

--- startup-cpt-timeline.php
+++ startup-cpt-timeline.php
@@ -134,7 +134,7 @@ if ( !function_exists( 'cmb2_detection' ) ) {
 }
 
 function startup_cpt_timeline_meta() {
-    require get_template_directory() . '/inc/font-awesome.php';
+    require ABSPATH . 'wp-content/plugins/startup-cpt-timeline/inc/font-awesome.php';
     
 	// Start with an underscore to hide fields from custom fields list
 	$prefix = '_startup_cpt_timeline_';
@@ -207,7 +207,7 @@ add_shortcode( 'timeline', 'startup_cpt_timeline_shortcode' );
 
 // Shortcode UI
 /**
- * Detecion de Shortcake. Identique dans tous les plugins.
+ * Detection de Shortcake. Identique dans tous les plugins.
  */
 if ( !function_exists( 'shortcode_ui_detection' ) ) {
     function shortcode_ui_detection() {
